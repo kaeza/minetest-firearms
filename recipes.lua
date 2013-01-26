f41318a22f16456fac8cdbad18bd32e8dd32aba3
Diego Martínez <kaeza@users.sf.net>
2013-01-26 21:21:40 -0200

    Fixed formatting in recipes

--- recipes.lua
+++ recipes.lua
@@ -3,43 +3,43 @@
 --
 
 minetest.register_craft({
-  output = 'firearms:he_40mm',
+  output = 'firearms:he_40mm';
   recipe = {
-    {'', 'default:steel_ingot', ''},
-    {'default:steel_ingot', 'bucket:bucket_lava', 'default:steel_ingot'},
-    {'', 'default:steel_ingot', ''},
-  },
-  replacements = { {"bucket:bucket_lava", "bucket:bucket_empty"} },
-})
+    { '', 'default:steel_ingot', '' },
+    { 'default:steel_ingot', 'bucket:bucket_lava', 'default:steel_ingot' },
+    { '', 'default:steel_ingot', '' },
+  };
+  replacements = { { "bucket:bucket_lava", "bucket:bucket_empty" } };
+});
 
 minetest.register_craft({
-  output = 'firearms:rocket',
+  output = 'firearms:rocket';
   recipe = {
-    {'default:steel_ingot', 'bucket:bucket_lava', 'default:steel_ingot'},
-  },
-  replacements = { {"bucket:bucket_lava", "bucket:bucket_empty"} },
-})
+    { 'default:steel_ingot', 'bucket:bucket_lava', 'default:steel_ingot' },
+  };
+  replacements = { { "bucket:bucket_lava", "bucket:bucket_empty" } };
+});
 
 minetest.register_craft({
-  output = 'firearms:bullet_45 10',
+  output = 'firearms:bullet_45 10';
   recipe = {
-    {'default:steel_ingot', 'default:steel_ingot'},
-  },
-})
+    { 'default:steel_ingot', 'default:steel_ingot' },
+  };
+});
 
 minetest.register_craft({
-  output = 'firearms:bullet_556 10',
+  output = 'firearms:bullet_556 10';
   recipe = {
     {'default:steel_ingot', 'default:steel_ingot', 'default:leaves'},
-  },
+  };
 })
 
 minetest.register_craft({
   output = 'firearms:shell_12 8',
   recipe = {
-    {'default:steel_ingot', '', ''},
-    {'default:steel_ingot', 'default:steel_ingot', 'default:steel_ingot'},
-    {'default:steel_ingot', '', ''},
+    { 'default:steel_ingot', '', '' },
+    { 'default:steel_ingot', 'default:steel_ingot', 'default:steel_ingot' },
+    { 'default:steel_ingot', '', '' },
   },
 })
 
@@ -48,6 +48,7 @@ minetest.register_craft({
 --
 
 minetest.register_craft({
+<<<<<<< Updated upstream
   output = 'firearms:pistol_45',
   recipe = {
     {'firearms:bullet_45', 'default:steel_ingot', 'default:steel_ingot'},
@@ -58,6 +59,18 @@ minetest.register_craft({
 
 minetest.register_craft({
   output = 'firearms:m79',
+=======
+  output = 'firearms:pistol_45';
+  recipe = {
+    {'firearms:bullet_45', 'default:steel_ingot', 'default:steel_ingot'},
+    {'', 'default:stick', 'deafutl:wood'},
+    {'', '', 'deafault:stick'},
+  };
+});
+
+minetest.register_craft({
+  output = 'firearms:m79';
+>>>>>>> Stashed changes
   recipe = {
     {'firearms:he_40mm', 'default:steel_ingot', 'default:steel_ingot'},
     {'', 'default:stick', 'default:wood'},
